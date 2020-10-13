b58457244fbc87db1086eea7dd91cc687cc0998d
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-13 11:03:09 +0530

    [Shreya] Choosing the letter

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace TicTacToeGame
 {
@@ -7,17 +8,25 @@ namespace TicTacToeGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to TicTacToe Game");
-            char[] board = boardCreation();
+            char[] board = BoardCreation();
+            char playerLetter = choosePlayerLetter();
         }
-            private static char[] boardCreation()
+        private static char[] BoardCreation()
+        {
+            char[] board = new char[10];
+            for (int i = 0; i < board.Length; i++)
             {
-                char[] board = new char[10];
-                for (int i = 0; i < board.Length; i++)
-                {
-                    board[i] = ' ';
-                }
-                return board;
+                board[i] = ' ';
+            }
+            return board;
+        }
+            public static char choosePlayerLetter()
+            {
+                Console.WriteLine("Choose Letter: ");
+                string playerLetter = Console.ReadLine();
+                return char.ToUpper(playerLetter[0]);
             }
         }
     }
+
 
