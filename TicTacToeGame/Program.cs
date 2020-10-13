bfa5b05c6de19b9504e3e8990e96e44f3ed94e30
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-13 11:12:20 +0530

    [Shreya] Displaying the game board

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -10,6 +10,7 @@ namespace TicTacToeGame
             Console.WriteLine("Welcome to TicTacToe Game");
             char[] board = BoardCreation();
             char playerLetter = choosePlayerLetter();
+            showBoard(board);
         }
         private static char[] BoardCreation()
         {
@@ -26,6 +27,14 @@ namespace TicTacToeGame
                 string playerLetter = Console.ReadLine();
                 return char.ToUpper(playerLetter[0]);
             }
+        private static void showBoard(char[] board)
+        {
+            Console.WriteLine("\n " + board[1] + " | " + board[2] + " | " + board[3]);
+            Console.WriteLine("---");
+            Console.WriteLine("\n " + board[4] + " | " + board[5] + " | " + board[6]);
+            Console.WriteLine("---");
+            Console.WriteLine("\n " + board[7] + " | " + board[8] + " | " + board[9]);
+        }
         }
     }
 
