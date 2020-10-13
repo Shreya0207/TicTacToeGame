8aa0a8975c4533aca88f2a8ef7a7f9895a5efb8d
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-13 10:27:12 +0530

    [Shreya] Creating the board

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe Game");
+            char[] board = boardCreation();
+        }
+            private static char[] boardCreation()
+            {
+                char[] board = new char[10];
+                for (int i = 0; i < board.Length; i++)
+                {
+                    board[i] = ' ';
+                }
+                return board;
+            }
+        }
+    }
+
